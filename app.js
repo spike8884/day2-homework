05c2ec2b1094464cdf2bdacf22fc7e15c769d6f1
Richard <spike8884@gmail.com>
2024-04-25 16:23:05 -0400

    homework

---
+++ app.js
@@ -0,0 +1,22 @@
+var x = 10;
+var y = 2;
+
+function addnumbers(param1, param2){
+    return param1 + param2
+}
+console.log(addnumbers(x, y))
+
+function difference(param1, param2){
+    return param1 - param2;
+}
+console.log(difference(y, x))
+
+function multiply(param1, param2){
+    return param1 * param2;
+}
+console.log(multiply(x, y))
+
+function divides(param1, param2){
+    return param1 / param2;
+}
+console.log(divides(x, y))
